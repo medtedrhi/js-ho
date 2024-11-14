92f6f0b5f2f1c049dcf8f8aa286419669378dc05
@med.tedrhi <med.tedrhi@gmail.com>
2024-11-14 20:56:24 +0100

    3

--- gallery.js
+++ gallery.js
@@ -1,25 +1,13 @@
-/*Name this external file gallery.js*/
+function upDate(previewPic) {
+   // Change the background image of the div with id "image" to the src of the preview image
+   document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
+   // Change the text of the div with id "image" to the alt text of the preview image
+   document.getElementById('image').innerHTML = previewPic.alt;
+}
 
-function upDate(previewPic){
-    /* In this function you should 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image*/
-       document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
-       /*2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
-       document.getElementById('image').innerHTML = previewPic.alt;
-     
-       }
-   
-       function unDo(){
-        /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was*/
-       
-    document.getElementById('image').style.backgroundImage = "url('')";
-     /*  2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
-       document.getElementById('image').innerHTML = "Hover over an image below to display here.";
-       }
+function unDo() {
+   // Reset the background image of the div with id "image"
+   document.getElementById('image').style.backgroundImage = "url('')";
+   // Reset the text of the div with id "image" to the default message
+   document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+}
